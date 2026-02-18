b0e4bc68c10b16a54085fe4ca444e900714f4916
Julian-M7 <skillsmaster56@gmail.com>
2026-02-18 16:57:14 -0500

    nodos

--- src/co/edu/uptc/model/ManagerList.java
+++ src/co/edu/uptc/model/ManagerList.java
@@ -1,11 +1,15 @@
 package co.edu.uptc.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class ManagerList {
+public class ManagerList implements Collection {
     Node header = null;
-    LinkedList<Product> listProduct = new LinkedList<>();
+    ArrayList<Product> listProduct = new ArrayList<>();
+    //Node product;
 
     private Node createNode(Product product){
         return new Node(product);
@@ -18,6 +22,7 @@ public class ManagerList {
         }
         return last;
     }
+
     public void addInicio(Product product){
         Node aux = new Node(product);
         if(header == null){
@@ -36,6 +41,20 @@ public class ManagerList {
             last.sig = aux;
         }
     }
+
+    private void rebuildListFromArray() {
+        header = null;
+        for (Product p : listProduct) {
+            Node aux = new Node(p);
+            if(header == null){
+                header = aux;
+            }else{
+                Node last = returnlastNode();
+                last.sig = aux;
+            }
+        }
+    }
+
     public void addEnd(Product product){
         Node aux = new Node(product);
         //conectarNodoAlFinal(product);
@@ -45,6 +64,7 @@ public class ManagerList {
             Node last = returnlastNode();
             last.sig = aux;
         }
+        listProduct.add(product);
     }
 
     public void showList(){
@@ -53,25 +73,106 @@ public class ManagerList {
             System.out.println(aux.product);
             aux = aux.sig;
         }
-        
     }
 
     public void addProduct(Product product) {
-        listProduct.addLast(product);
+        //listProduct.addLast(product);
+        addEnd(product);
     }
 
     public void showProduct() {
-        for (Product p : listProduct) {
-            System.out.println(p);
-        }
+        //for (Product p : listProduct) {
+          //  System.out.println(p);
+        //}
+        showList();
     }
 
     public void sortByDescription() {
-        listProduct.sort((p1, p2) -> p1.getDescription().compareToIgnoreCase(p2.getDescription()));
+        //listProduct.sort((p1, p2) -> p1.getDescription().compareToIgnoreCase(p2.getDescription()));
+        Collections.sort(listProduct, (p1, p2) -> 
+            p1.getDescription().compareToIgnoreCase(p2.getDescription()));
+        rebuildListFromArray();
     }
 
-    public void sortByName() {
-        listProduct.sort((p1, p2) -> p1.getDescription().compareToIgnoreCase(p2.getDescription()));
+    public void sortByPrice() {
+        Collections.sort(listProduct, (p1, p2) -> 
+            p1.getPrice().compareToIgnoreCase(p2.getPrice()));
+        rebuildListFromArray();
+    }
+
+    @Override
+    public int size() {
+        return listProduct.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return listProduct.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return listProduct.contains(o);
+    }
+
+    @Override
+    public Iterator iterator() {
+        return listProduct.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return listProduct.toArray();
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return listProduct.toArray(a);
+    }
+
+    @Override
+    public boolean add(Object e) {
+        if (e instanceof Product) {
+            addProduct((Product) e);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'containsAll'");
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addAll'");
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeAll'");
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'retainAll'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
     }
 
     
